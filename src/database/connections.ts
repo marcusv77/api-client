89383579faa5f94000a4aaf87860127b0f77af28
marcusv77 <marcus.vsf@aluno.ufop.edu.br>
2020-12-06 22:45:20 -0300

    Preparing deploy

--- src/database/connections.ts
+++ src/database/connections.ts
@@ -1,4 +1,4 @@
-import knex from  'knex';
+import knex from 'knex';
 import 'dotenv/config'
 
 const db = knex({
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -4,8 +4,8 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
